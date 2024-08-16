2270badb3135203632887432f2bc34f36278fdf8
idowuoluwasanmi <idowuoluwasanmi@gmail.com>
2024-08-16 19:43:53 +0100

    new changes to azure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const sql = require('mssql');
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
