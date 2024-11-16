2d5db122a272a93eda90a8d8bedd2d282fa5c286
Oluwasanmi <idowu@Oluwasanmi-PC>
2024-11-16 18:40:51 +0000

    new note look

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Database configuration for Azure SQL Database
 const dbConfig = {
